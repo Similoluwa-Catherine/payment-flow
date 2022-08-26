c33860bc4a1ce56429f1f6e0e1eb7bc481242fec
Similoluwa-Catherine <catherinesimilolwa@gmail.com>
2022-08-26 19:45:58 +0100

    fixes

--- src/Nav.js
+++ src/Nav.js
@@ -5,23 +5,31 @@ const Nav = () => {
     <header>
       <nav className="mt-4">
         <h4>Complete your Purchase</h4>
-        <div className="nav-container d-flex justify-content-between list-unstyled mt-3">
-          <NavLink className="nav-link fw-bold" to={`/`}
-            style={({ isActive }) => ({ color: isActive ? 'hsl(28, 87%, 62%)' : 'hsl(0, 0%, 74%)' })}
-          >
-            Personal Info
-          </NavLink>
-          <NavLink className="nav-link fw-bold" to={`/billing`}
-            style={({ isActive }) => ({ color: isActive ? 'hsl(28, 87%, 62%)' : 'hsl(0, 0%, 74%)' })}
-          >
-            Billing Info
-          </NavLink>
-          <NavLink className="nav-link" to={`/payment-confirmation`}
-            style={({ isActive }) => ({ color: isActive ? 'hsl(28, 87%, 62%)' : 'hsl(0, 0%, 74%)' })}
-          >
-            Confirm Payment
-          </NavLink>
-        </div>
+        <ul className="nav-container d-flex justify-content-between list-unstyled mt-3">
+          <li>
+            <NavLink
+              className="nav-link fw-bold"
+              to={`/`}
+              style={({ isActive }) => ({
+                color: isActive ? "hsl(28, 87%, 62%)" : "hsl(0, 0%, 74%)",
+              })}
+            >
+              Personal Info
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="nav-link fw-bold"
+              to={`/billing`}
+              style={({ isActive }) => ({
+                color: isActive ? "hsl(28, 87%, 62%)" : "hsl(0, 0%, 74%)",
+              })}
+            >
+              Billing Info
+            </NavLink>
+          </li>
+          <li style={{color: "hsl(0, 0%, 74%)", fontWeight: 700}}>Confirm Payment</li>
+        </ul>
       </nav>
     </header>
   );
